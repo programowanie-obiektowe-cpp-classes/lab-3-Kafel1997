5d9a6c767900ae15e8f730b96c3a5811f4a44f84
Kafel1997 <stanislaw.sufin@gmail.com>
2024-11-14 14:07:58 +0100

    wykonano zadanie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,31 @@
 #pragma once
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0; 
+
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow    = 0;
+    Stocznia     stocznia;
+
+    while (przetransportowano < towar) {
+        Statek*      statek              = stocznia();
+        unsigned int transportowanaIlosc = statek->transportuj();
+        przetransportowano += transportowanaIlosc;
+
+        
+        if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+            liczbaZaglowcow++;
+        }
+
+        delete statek; 
+    }
+
+    return liczbaZaglowcow;
 }
